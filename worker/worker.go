1fb9ee985e6a10a5eb516bd023e16568c66f730c
eric <btstarinfo@gmail.com>
2020-02-08 19:43:56 +0800

    start worker punctually

--- worker/worker.go
+++ worker/worker.go
@@ -21,9 +21,13 @@ func GetDefaultDepthSize() int {
 	return defaultDepthSize
 }
 
-func NewSpotDepthWorker(ctx context.Context, depthData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair, period time.Duration) {
-	log.Printf("new spot depth worker for [%s] %s, period is %dms", exchange, pair.String(), period/time.Millisecond)
+func prepare() {
+	now := time.Now()
+	diff := int(time.Second) - now.Nanosecond()
+	time.Sleep(time.Duration(diff))
+}
 
+func updateSportDepth(depthData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair) {
 	dep, err := api.GetDepth(defaultDepthSize, pair)
 	if err != nil {
 		log.Printf("[%s] refresh %s depth error:%s", exchange, pair.String(), err.Error())
@@ -36,58 +40,18 @@ func NewSpotDepthWorker(ctx context.Context, depthData *data.Data, api goex.API,
 		}
 		depthData.UpdateSpotDepth(exchange, pair.String(), dep)
 	}
-
-	ticker := time.NewTicker(period)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			dep, err := api.GetDepth(defaultDepthSize, pair)
-			if err != nil {
-				log.Printf("[%s] refresh %s depth error:%s", exchange, pair.String(), err.Error())
-			} else {
-				if dep.AskList[0].Price > dep.AskList[1].Price {
-					sort.Sort(dep.AskList)
-				}
-				if dep.BidList[0].Price < dep.BidList[1].Price {
-					sort.Sort(sort.Reverse(dep.AskList))
-				}
-				depthData.UpdateSpotDepth(exchange, pair.String(), dep)
-			}
-		}
-	}
 }
 
-func NewSpotTickerWorker(ctx context.Context, tickerData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair, period time.Duration) {
-	log.Printf("new spot ticker worker for [%s] %s, period is %dms ", exchange, pair.String(), period/time.Millisecond)
-
+func updateSportTicker(tickerData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair) {
 	tick, err := api.GetTicker(pair)
 	if err != nil {
 		log.Printf("[%s] refresh %s ticker error:%s", exchange, pair.String(), err.Error())
 	} else {
 		tickerData.UpdateSpotTicker(exchange, pair.String(), tick)
 	}
-
-	ticker := time.NewTicker(period)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			tick, err := api.GetTicker(pair)
-			if err != nil {
-				log.Printf("[%s] refresh %s ticker error:%s", exchange, pair.String(), err.Error())
-			} else {
-				tickerData.UpdateSpotTicker(exchange, pair.String(), tick)
-			}
-		}
-	}
 }
 
-func NewFutureDepthWorker(ctx context.Context, depthData *data.Data, api goex.FutureRestAPI, exchange, contactType string, pair goex.CurrencyPair, period time.Duration) {
-	log.Printf("new future depth worker for [%s] %s %s, period is %dms", exchange, contactType, pair.String(), period/time.Millisecond)
-
+func updateFutureDepth(depthData *data.Data, api goex.FutureRestAPI, exchange, contactType string, pair goex.CurrencyPair) {
 	dep, err := api.GetFutureDepth(pair, contactType, defaultDepthSize)
 	if err != nil {
 		log.Printf("[%s] %s refresh %s depth error:%s", exchange, contactType, pair.String(), err.Error())
@@ -100,6 +64,54 @@ func NewFutureDepthWorker(ctx context.Context, depthData *data.Data, api goex.Fu
 		}
 		depthData.UpdateFutureDepth(exchange, contactType, pair.String(), dep)
 	}
+}
+
+func updateFutureTicker(tickerData *data.Data, api goex.FutureRestAPI, exchange, contactType string, pair goex.CurrencyPair) {
+	tick, err := api.GetFutureTicker(pair, contactType)
+	if err != nil {
+		log.Printf("[%s] %s refresh %s ticker error:%s", exchange, contactType, pair.String(), err.Error())
+	} else {
+		tickerData.UpdateFutureTicker(exchange, contactType, pair.String(), tick)
+	}
+}
+
+func NewSpotDepthWorker(ctx context.Context, depthData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair, period time.Duration) {
+	log.Printf("new spot depth worker for [%s] %s, period is %dms", exchange, pair.String(), period/time.Millisecond)
+
+	updateSportDepth(depthData, api, exchange, pair)
+	prepare()
+	ticker := time.NewTicker(period)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			updateSportDepth(depthData, api, exchange, pair)
+		}
+	}
+}
+
+func NewSpotTickerWorker(ctx context.Context, tickerData *data.Data, api goex.API, exchange string, pair goex.CurrencyPair, period time.Duration) {
+	log.Printf("new spot ticker worker for [%s] %s, period is %dms ", exchange, pair.String(), period/time.Millisecond)
+
+	updateSportTicker(tickerData, api, exchange, pair)
+	prepare()
+	ticker := time.NewTicker(period)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			updateSportTicker(tickerData, api, exchange, pair)
+		}
+	}
+}
+
+func NewFutureDepthWorker(ctx context.Context, depthData *data.Data, api goex.FutureRestAPI, exchange, contactType string, pair goex.CurrencyPair, period time.Duration) {
+	log.Printf("new future depth worker for [%s] %s %s, period is %dms", exchange, contactType, pair.String(), period/time.Millisecond)
+
+	updateFutureDepth(depthData, api, exchange, contactType, pair)
+	prepare()
 
 	ticker := time.NewTicker(period)
 	for {
@@ -107,18 +119,7 @@ func NewFutureDepthWorker(ctx context.Context, depthData *data.Data, api goex.Fu
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			dep, err := api.GetFutureDepth(pair, contactType, defaultDepthSize)
-			if err != nil {
-				log.Printf("[%s] %s refresh %s depth error:%s", exchange, contactType, pair.String(), err.Error())
-			} else {
-				if dep.AskList[0].Price > dep.AskList[1].Price {
-					sort.Sort(dep.AskList)
-				}
-				if dep.BidList[0].Price < dep.BidList[1].Price {
-					sort.Sort(sort.Reverse(dep.AskList))
-				}
-				depthData.UpdateFutureDepth(exchange, contactType, pair.String(), dep)
-			}
+			updateFutureDepth(depthData, api, exchange, contactType, pair)
 		}
 	}
 }
@@ -126,12 +127,8 @@ func NewFutureDepthWorker(ctx context.Context, depthData *data.Data, api goex.Fu
 func NewFutureTickerWorker(ctx context.Context, tickerData *data.Data, api goex.FutureRestAPI, exchange, contactType string, pair goex.CurrencyPair, period time.Duration) {
 	log.Printf("new future ticker worker for [%s] %s %s, period is %dms ", exchange, contactType, pair.String(), period/time.Millisecond)
 
-	tick, err := api.GetFutureTicker(pair, contactType)
-	if err != nil {
-		log.Printf("[%s] %s refresh %s ticker error:%s", exchange, contactType, pair.String(), err.Error())
-	} else {
-		tickerData.UpdateFutureTicker(exchange, contactType, pair.String(), tick)
-	}
+	updateFutureTicker(tickerData, api, exchange, contactType, pair)
+	prepare()
 
 	ticker := time.NewTicker(period)
 	for {
@@ -139,12 +136,7 @@ func NewFutureTickerWorker(ctx context.Context, tickerData *data.Data, api goex.
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			tick, err := api.GetFutureTicker(pair, contactType)
-			if err != nil {
-				log.Printf("[%s] %s refresh %s ticker error:%s", exchange, contactType, pair.String(), err.Error())
-			} else {
-				tickerData.UpdateFutureTicker(exchange, contactType, pair.String(), tick)
-			}
+			updateFutureTicker(tickerData, api, exchange, contactType, pair)
 		}
 	}
 }
